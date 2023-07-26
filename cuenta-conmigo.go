2ae62f008b2859b0b76710943d646de4a41e15b0
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-07-26 10:03:06 -0400

    first try press key, print ascii letter

--- cuenta-conmigo.go
+++ cuenta-conmigo.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	letterA = `
+  A
+ A A
+AAAAA
+A   A
+A   A
+`
+	letterB = `
+BBBB
+B   B
+BBBB
+B   B
+BBBB
+`
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println("Hola")
+
+	var inputLetter string
+	fmt.Print("Ingresa una letra: ")
+	fmt.Scanln(&inputLetter)
+
+	switch inputLetter {
+	case "A", "a":
+		fmt.Println(letterA)
+	case "B", "b":
+		fmt.Println(letterB)
+	// Agrega más casos para otras letras aquí
+	default:
+		fmt.Println("La letra ingresada no tiene una imagen ASCII asociada.")
+	}
+
 }
