d0368fd5815b994cf2903221aa9df691e8614ed6
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-07-27 08:49:06 -0400

    create for loop

--- cuenta-conmigo.go
+++ cuenta-conmigo.go
@@ -10,32 +10,74 @@ const (
  A A
 AAAAA
 A   A
-A   A
-`
+A   A`
 	letterB = `
-BBBB
-B   B
-BBBB
-B   B
-BBBB
-`
+ 	Braquiosaurio
+
+	  _
+ .~q \,
+{__,  \
+    \' \
+     \  \
+      \  \
+       \  \._            __.__
+        \    ~-._  _.==~~     ~~--.._
+         \        '                  ~-.
+          \      _-   -_                \.
+           \    /       }        .-    .  \
+            \. |      /  }      (       ;  \
+              \|     /  /       (       :    \
+               \    |  /        |      /       \    =
+                |     /\-.______.\     |^-.      \
+                |   |/           (     |    .      \_
+                |   ||            ~\   \      '._    \-.._____..----.._=__
+                |   |/             _\   \      =~-.__________.-~~~~~~~~~'''
+              .o'___/            .o______}`
+	letterT = `
+	Triceratops
+                       _. - ~ ~ ~ - .
+   ..       __.    .-~               ~-.
+   ((\     /   \}.~                     \.
+    \\\   {     }               /     \   \
+(\   \\~~^      }              |       }   \
+ \\.-~ -@~      }  ,-.         |       )    \
+ (___     ) _}   (    :        |    / /      \.
+  \----._-~.     _\ \ |_       \   / /- _      \.
+         ~~----~~  \ \| ~~--~~~(  + /     ~-.     ~- _
+                   /  /         \  \          ~- . _ _~_-_.
+                __/  /          _\  )
+              .<___.'         .<___/`
 )
 
 func main() {
-	fmt.Println("Hola")
+	fmt.Println("Había una vez, en un lugar muy muy lejano. Un...")
 
 	var inputLetter string
-	fmt.Print("Ingresa una letra: ")
-	fmt.Scanln(&inputLetter)
 
-	switch inputLetter {
-	case "A", "a":
-		fmt.Println(letterA)
-	case "B", "b":
-		fmt.Println(letterB)
-	// Agrega más casos para otras letras aquí
-	default:
-		fmt.Println("La letra ingresada no tiene una imagen ASCII asociada.")
+	for {
+		fmt.Print("Ingresa una letra o 'fin' para terminar: ")
+		fmt.Scanln(&inputLetter)
+
+		if inputLetter == "fin" {
+			break
+		}
+
+		switch inputLetter {
+		case "A", "a":
+			fmt.Println(letterA)
+		case "B", "b":
+			fmt.Println(letterB)
+		case "T", "t":
+			fmt.Println(letterT)
+
+		default:
+			fmt.Println("La letra ingresada no tiene una imagen ASCII asociada.")
+		}
+
+		fmt.Println("Ingresa otra letra para ver con qué, quién o qué lugar se encuentra nuestro personaje (o escribe 'fin' para terminar): ")
+
 	}
+
+	fmt.Println("¡Hasta luego!")
 
 }
